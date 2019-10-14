1bf705264358d53f5003ab1e98cc59e0728ae717
morrisrobert <morrisrobert@outlook.com>
2019-10-14 14:21:03 -0500

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -63,9 +63,8 @@
 
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
 app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+app.all('*', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/index.html');
+});
+app.listen(process.env.PORT || 8080); 
