6e5f728cd8e121cf6b0036c6b50bd9578ef3e8bd
morrisrobert <morrisrobert@outlook.com>
2019-10-14 14:16:33 -0500

    updating express configs

--- webserver.js
+++ webserver.js
@@ -59,19 +59,13 @@
 // console.log('server started');
 
 
-//Install express server
+
+
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<MKE-Flavors>'));
-
-app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname + '/dist/MKE-Flavors/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
