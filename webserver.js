73710e3da92a6c64c7f0b29c81908889d1de4926
morrisrobert <morrisrobert@outlook.com>
2019-10-14 14:09:02 -0500

    added express listener

--- webserver.js
+++ webserver.js
@@ -1,95 +1,77 @@
-const Express = require("express");
-const BodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
+// const Express = require("express");
+// const BodyParser = require("body-parser");
+// const MongoClient = require("mongodb").MongoClient;
+// const ObjectId = require("mongodb").ObjectID;
 
-const CONNECTION_URL = "mongodb+srv://***REMOVED***@cluster0-i9wli.mongodb.net/test?retryWrites=true&w=majority";
-const DATABASE_NAME = "mkeflavors";
+// const CONNECTION_URL = "mongodb+srv://***REMOVED***@cluster0-i9wli.mongodb.net/test?retryWrites=true&w=majority";
+// const DATABASE_NAME = "mkeflavors";
 
-var app = Express();
+// var app = Express();
 
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
+// app.use(BodyParser.json());
+// app.use(BodyParser.urlencoded({ extended: true }));
 
-var database, collection;
+// var database, collection;
 
-// const LocationModel = MongoClient.model("location", {
-//     name: String,
-//     address: String,
-//     city: String,
-//     state: String,
-//     zip: String,
-//     URL: String,
-//     lat: String,
-//     long: String,
-//     flavorSelector: String, 
-//     descriptionSelector: String,
-//     flavors: String,
-//     descriptions: String
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
 // });
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.listen(3000, () => {
-    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useNewUrlParser: true }, (error, client) => {
-        if (error) {
-            throw error;
-        }
-        database = client.db(DATABASE_NAME);
-        collection = database.collection("locations");
-        // console.log("Connected to `" + DATABASE_NAME + "`!");
-    });
-});
-
-// app.route('/flavortest').get(function (request, response) {
-//     MongoClient.connect(CONNECTION_URL, function (error, client) {
+// app.listen(3000, () => {
+//     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useNewUrlParser: true }, (error, client) => {
+//         if (error) {
+//             throw error;
+//         }
 //         database = client.db(DATABASE_NAME);
 //         collection = database.collection("locations");
-//         var cursor = collection.find();
-//         //noinspection JSDeprecatedSymbols
-//         cursor.each(function (err, item) {
-//             if (item != null) {
-//                 str = str + "  Name  " + item.name + item.flavors + "</br>";
-//                 // console.log(item.name);
-//             } else {
-//                 console.log('empty found');
-//             }
-//         });
-//         response.send(str);
-//         console.log(str);
-//         // database.close();
 //     });
 // });
 
-app.post("/api/location", (request, response) => {
-    collection.insert(request.body, (error, result) => {
-        if (error) {
-            return response.status(500).send(error);
-        }
-        response.send(result.result);
-    });
+// app.post("/api/location", (request, response) => {
+//     collection.insert(request.body, (error, result) => {
+//         if (error) {
+//             return response.status(500).send(error);
+//         }
+//         response.send(result.result);
+//     });
+// });
+
+// app.get("/api/locations", (request, response) => {
+//     collection.find({}).toArray((error, result) => {
+//         if (error) {
+//             return response.status(500).send(error);
+//         }
+//         response.send(result);
+//     });
+// });
+
+// app.get("/api/location/:id", (request, response) => {
+//     collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+//         if (error) {
+//             return response.status(500).send(error);
+//         }
+//         response.send(result);
+//     });
+// });
+
+// console.log('server started');
+
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<MKE-Flavors>'));
+
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/MKE-Flavors/index.html'));
 });
 
-app.get("/api/locations", (request, response) => {
-    collection.find({}).toArray((error, result) => {
-        if (error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
-});
-
-app.get("/api/location/:id", (request, response) => {
-    collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
-        if (error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
-});
-
-console.log('server started');
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
