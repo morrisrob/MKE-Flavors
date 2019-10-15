913d3b06aab026f80d583a49fb5f116258f0b291
morrisrobert <morrisrobert@outlook.com>
2019-10-14 21:44:53 -0500

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -104,6 +104,23 @@ const DATABASE_NAME = "mkeflavors";
 let database;
 let collection;
 
+MongoClient.connect(process.env.MONGODB_URI || CONNECTION_URL, function (err, location) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    // Save database object from the callback for reuse.
+    database = location.db();
+    console.log("Database connection ready");
+
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || 8080, function () {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
+});
+
 // Serve static files
 app.use(Express.static(__dirname + '/dist/MKE-Flavors'));
 
@@ -113,7 +130,7 @@ app.get('/*', function (req, res) {
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+// app.listen(process.env.PORT || 5000);
 
 // MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useNewUrlParser: true }, (error, client) => {
 //     if (error) {
