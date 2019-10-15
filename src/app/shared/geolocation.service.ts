379f1e305f28c5809280910597d9f525955f98ae
morrisrobert <morrisrobert@outlook.com>
2019-10-15 10:52:13 -0500

    Update geolocation.service.ts

--- src/app/shared/geolocation.service.ts
+++ src/app/shared/geolocation.service.ts
@@ -39,14 +39,12 @@ export class GeolocationService {
 
   getPosition(): Promise<any> {
     return new Promise((resolve, reject) => {
-
       navigator.geolocation.getCurrentPosition(resp => {
-
         resolve({ lng: resp.coords.longitude, lat: resp.coords.latitude });
       },
         err => {
           reject(err);
-        });
+        }, {timeout: 5000});
     });
 
   }
