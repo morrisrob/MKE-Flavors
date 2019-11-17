11a5dad4b2413c44ec0fe675e4bfd4c26d1ec854
morrisrobert <morrisrobert@outlook.com>
2019-11-16 21:46:29 -0600

    add functionality to pull from flavor calendar in addition to scraping

--- getflavors.js
+++ getflavors.js
@@ -6,6 +6,22 @@ const DATABASE_NAME = "mkeflavors";
 
 let database, collection;
 
+function getCurrentDate() {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; //January is 0
+
+  let yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+  today = mm + "/" + dd + "/" + yyyy;
+  return today;
+}
+
 function getFlavorsDB() {
   MongoClient.connect(
     CONNECTION_URL,
@@ -21,31 +37,47 @@ function getFlavorsDB() {
       cursor.each(function(err, item) {
         let flavorArray = [];
         let descriptionArray = [];
+        let today = getCurrentDate();
+
         if (item != null) {
-          axios.get(item.URL).then(response => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-            const selectorArray = item.flavorSelector;
-            const descriptionSelectorArray = item.descriptionSelector;
-            const selectorArrayLength = selectorArray.length;
-            for (let i = 0; i < selectorArrayLength; i++) {
-              let flavor = $(selectorArray[i]).text();
-              if (flavor.includes(":") === true) {
-                let flavorSplit = flavor.split(":");
-                flavor = flavorSplit[1].toString();
-              }
-              flavor = flavor.trim();
-              const description = $(descriptionSelectorArray[i]).text();
-              console.log(item.name + " flavor is " + flavor);
-              console.log(item.name + " description is " + description);
-              flavorArray.push(flavor);
-              descriptionArray.push(description);
+          if (item.scrapedSite != true) {
+            if (item.flavorCal[today]) {
+              let flavors = [];
+              flavors.push(item.flavorCal[today]);
+              collection.findOneAndUpdate(
+                { name: item.name },
+                { $set: { flavors: flavors } }
+              );
+              console.log("not scraped");
             }
-            collection.findOneAndUpdate(
-              { name: item.name },
-              { $set: { flavors: flavorArray, descriptions: descriptionArray } }
-            );
-          });
+          } else {
+            axios.get(item.URL).then(response => {
+              const html = response.data;
+              const $ = cheerio.load(html);
+              const selectorArray = item.flavorSelector;
+              const descriptionSelectorArray = item.descriptionSelector;
+              const selectorArrayLength = selectorArray.length;
+              for (let i = 0; i < selectorArrayLength; i++) {
+                let flavor = $(selectorArray[i]).text();
+                if (flavor.includes(":") === true) {
+                  let flavorSplit = flavor.split(":");
+                  flavor = flavorSplit[1].toString();
+                }
+                flavor = flavor.trim();
+                const description = $(descriptionSelectorArray[i]).text();
+                console.log(item.name + " flavor is " + flavor);
+                console.log(item.name + " description is " + description);
+                flavorArray.push(flavor);
+                descriptionArray.push(description);
+              }
+              collection.findOneAndUpdate(
+                { name: item.name },
+                {
+                  $set: { flavors: flavorArray, descriptions: descriptionArray }
+                }
+              );
+            });
+          }
         } else {
           console.log("empty found");
         }
