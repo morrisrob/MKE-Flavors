b97b684fc91acd4c528b2b899bd8dabcd150e003
morrisrobert <morrisrobert@outlook.com>
2019-11-26 20:01:23 -0600

    Delete routes.js

--- server/routes/routes.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const path = require("path");
-
-router.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/../dist/MKE-Flavors/index.html"));
-});
-
-module.exports = router;
